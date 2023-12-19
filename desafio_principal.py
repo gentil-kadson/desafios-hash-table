3652a7e6416ee1a9fee10e53dda9afa9d9c3775d
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2023-12-19 09:40:43 -0300

    desafio principal implementado.

--- desafio_principal.py
+++ desafio_principal.py
@@ -1,14 +1,36 @@
+from _thread import start_new_thread
 from random import randint
-from datetime import time
-hash_table = {}
+from datetime import datetime
+
+tabela_sensores = {}
 
 
-def store_sensors_data():
-    sensor_id = 1
+def resolver_conflito(tabela_sensores: dict[str], chave: str):
+    lista_sensores: list[tuple] = list(tabela_sensores.items())
+    while lista_sensores.count((chave, tabela_sensores[chave])) > 1:
+        indice_da_copia = lista_sensores.index((chave, tabela_sensores[chave]))
+        del lista_sensores[indice_da_copia]
+        lista_sensores = tuple(lista_sensores)
+        tabela_sensores = dict((x, y) for x, y in lista_sensores)
 
-    while True:
-        temperature = randint(26, 50) # ºC
-        pressure = randint(1, 5) # atm
-        humidity = randint(30, 120) # kg/m³
-        registration_time = time()
-        hash_table
+
+def cadastrar_sensor(id: int) -> None:
+    temperatura = randint(-40, 50)  # ºC
+    pressao = randint(1, 5)  # atm
+    umidade = randint(10, 60)  # %
+    leitura_sensor = {
+        "temperatura": temperatura,
+        "pressao": pressao,
+        "umidade": umidade
+    }
+
+    timestamp = str(datetime.now().timestamp())
+    chave = str(id) + timestamp
+    tabela_sensores[chave] = leitura_sensor
+    resolver_conflito(tabela_sensores, chave)
+
+
+id = 1
+while True:
+    runningThread = start_new_thread(cadastrar_sensor, (id,))
+    id = id + 1
